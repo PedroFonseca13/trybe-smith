b831538dde17c19915e796fd665c6725864baee6
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-13 18:18:33 -0300

    feat: validacoes para users

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
 import controller from '../controller/user.controller';
+import userMiddleware from '../middleware/user.middleware';
 
 const router = Router();
 
 router.post('/', controller.addUser);
+router.post(
+  '/',
+  userMiddleware.validateUsername,
+  userMiddleware.validateClasse,
+  userMiddleware.validateUserLevel,
+  userMiddleware.validateUserPassword,
+  controller.addUser,
+);
 
 export default router;
