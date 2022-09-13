a4e815b53b0efae2a31555d5d076ddd39845324f
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-13 18:40:18 -0300

    refactor: criada a funcao de login no controller do user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import IUser from '../interfaces/user.interface';
+import IUserCredentials from '../interfaces/userCredentials.interface';
 import services from '../services/user.services';
 
 const addUser = async (req: Request, res: Response) => {
@@ -15,4 +16,15 @@ const addUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { addUser };
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const getUser = req.body as IUserCredentials;
+    const user = await services.login(getUser);
+
+    return res.status(StatusCodes.OK).json({ token: user });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export default { addUser, login };
