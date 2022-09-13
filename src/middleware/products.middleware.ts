af17650ed27cdb9f698991e27da0732b5731243a
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-13 16:54:11 -0300

    feat: validacoes para produtos

---
+++ src/middleware/products.middleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+function validateName(req: Request, res: Response, next: NextFunction) {
+  const { name } = req.body;
+  if (name === undefined) {
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: '"name" is required' });
+  }
+  if (typeof name !== 'string') {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"name" must be a string' });
+  }
+  if (name.length < 3) {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"name" length must be at least 3 characters long' });
+  }
+  next();
+}
+
+function validateAmount(req: Request, res: Response, next: NextFunction) {
+  const { amount } = req.body;
+  if (amount === undefined) {
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: '"amount" is required' });
+  }
+  if (typeof amount !== 'string') {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"amount" must be a string' });
+  }
+  if (amount.length < 3) {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"amount" length must be at least 3 characters long' });
+  }
+  next();
+}
+
+export default { validateName, validateAmount };
--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import controller from '../controller/products.controller';
+import productsMiddleware from '../middleware/products.middleware';
 
 const router = Router();
 
 router.get('/', controller.getAll);
-router.post('/', controller.addProduct);
+router.post(
+  '/', 
+  productsMiddleware.validateName,
+  productsMiddleware.validateAmount,
+  controller.addProduct,
+);
 
 export default router;
